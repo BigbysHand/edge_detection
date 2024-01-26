5fe052f12e6c3a4c99054beb2c08e189e630649d
BigbysHand <rdbrhys1@gmail.com>
2024-01-26 19:51:53 +0000

    First Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+pub fn conv(_input: &[f32], _kernel: &[f32])->Vec<f32>
+{
+    let output: Vec<f32> = Vec::new();
+    return output;
+}
+
+fn main() 
+{
+
 }
